757ba710ae8ca3addccc928d63141ff9714f94a1
MarcusQuirino <marcusfilipus@gmail.com>
2025-01-02 16:33:49 -0300

    :3

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'xs': '475px',
+			},
+			container: {
+				screens: {
+					sm: '640px',
+					md: '768px',
+					lg: '1024px',
+					xl: '1280px',
+				}
+			}
+		},
 	},
 	plugins: [],
 }
